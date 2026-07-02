7c692ae3736e12ccb161efbc2f7865185d0ac377
Kirisigan Kannathasan <kkirisigan@gmail.com>
2026-07-02 08:24:50 +0530

    Create HelloWorld service interface

---
+++ src/main/java/com/helloworldrpc/server/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.helloworldrpc.server;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface HelloWorld {
+
+    @WebMethod
+    String sayHello(String name);
+
+}
